344f5d9ddb63bcb3cf7c416f700ed1feb8282824
Prabu-T <prabut463@gmail.com>
2024-05-28 01:33:03 +0530

    Reverting Cors to the backend

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,27 @@
 import express from "express"
 import mysql from "mysql"
 import cors from "cors"
+
 const app=express()
 app.use(express.json())
 app.use(cors())
 
-const allowedOrigins = ['https://loquacious-gecko-c5a5b7.netlify.app/'];
+// const allowedOrigins = ['https://loquacious-gecko-c5a5b7.netlify.app/'];
 
-app.use(cors({
-  origin: function(origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
+// app.use(cors({
+//   origin: function(origin, callback) {
+//     // Allow requests with no origin (like mobile apps or curl requests)
+//     if (!origin) return callback(null, true);
 
-    // Allow requests from the specified origins
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      return callback(null, true);
-    }
+//     // Allow requests from the specified origins
+//     if (allowedOrigins.indexOf(origin) !== -1) {
+//       return callback(null, true);
+//     }
 
-    // Block all other requests
-    return callback(new Error('Not allowed by CORS'));
-  }
-}));
+//     // Block all other requests
+//     return callback(new Error('Not allowed by CORS'));
+//   }
+// }));
 const db=mysql.createConnection({
     host:"localhost",
     user:"root",
